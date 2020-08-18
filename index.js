5ef6f1f1acbbe5ba004bd83c7f4cf7cf7ccf878e
Jaye Hernandez <jaye@dealgrocer.com>
2020-08-18 23:38:10 +0800

    Fix duplicate note

--- index.js
+++ index.js
@@ -2,28 +2,22 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const words = require('./words');
 
-const note = '\n\n![Janet Reminder](https://github.com/jayehernandez/the-good-place/blob/master/images/curse.png?raw=true)'
+const imageUrl = 'https://github.com/jayehernandez/the-good-place/blob/master/images/curse.png?raw=true';
+const note = `\n\n![Janet Reminder](${imageUrl})`;
 
 async function run() {
   try {
     const token = core.getInput('repo-token');
     const octokit = github.getOctokit(token);
 
-    console.log(words)
-
     if (github.context.eventName == 'issues') {
       let issue = github.context.payload.issue;
 
-      console.log(issue.title);
       let newTitle = checkString(issue.title);
       let newBody = checkString(issue.body);
 
-
-      console.log(newTitle);
-      console.log(newBody);
-
       if (issue.title !== newTitle || issue.body != newBody) {
-        if (!newBody.includes(note)) newBody += note;
+        if (!newBody.includes(imageUrl)) newBody += note;
 
         await octokit.issues.update({
           ...github.context.repo,
