5a4ab3416e468cd1c805b90ae3debb80f8168c19
Jaye Hernandez <jaye@dealgrocer.com>
2020-08-18 23:44:23 +0800

    Enclose in function :

--- index.js
+++ index.js
@@ -10,25 +10,42 @@ async function run() {
     const token = core.getInput('repo-token');
     const octokit = github.getOctokit(token);
 
-    if (github.context.eventName == 'issues') {
-      let issue = github.context.payload.issue;
-
-      let newTitle = checkString(issue.title);
+    if (github.context.eventName === 'issues') {
+      checkIssue(github, octokit);
+    } else if (github.context.eventName === 'issue_comment') {
+      let comment = github.context.payload.comment;
       let newBody = checkString(issue.body);
 
-      if (issue.title !== newTitle || issue.body != newBody) {
+      if (issue.body != newBody) {
         if (!newBody.includes(imageUrl)) newBody += note;
 
-        await octokit.issues.update({
+        await octokit.issues.updateComment({
           ...github.context.repo,
-          issue_number: issue.number,
-          title: newTitle,
+          comment_id: comment.id,
           body: newBody
         });
       }
     }
   } catch (error) {
     core.setFailed(error.message);
+  }
+}
+
+
+function checkIssue(github, octokit) {
+  let issue = github.context.payload.issue;
+  let newTitle = checkString(issue.title);
+  let newBody = checkString(issue.body);
+
+  if (issue.title !== newTitle || issue.body != newBody) {
+    if (!newBody.includes(imageUrl)) newBody += note;
+
+    await octokit.issues.update({
+      ...github.context.repo,
+      issue_number: issue.number,
+      title: newTitle,
+      body: newBody
+    });
   }
 }
 
