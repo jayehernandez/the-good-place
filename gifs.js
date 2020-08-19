d3f10edd69c0888cd2ebc88711f3d3ec33a6c99d
Jaye Hernandez <jaye@dealgrocer.com>
2020-08-19 22:04:43 +0800

    Add more gifs

--- gifs.js
+++ gifs.js
@@ -1,63 +1,71 @@
 module.exports = {
   bug: [
-    'https://media.giphy.com/media/fAn4sVk9oybFspoXt8/giphy.gif',
-    'https://media.giphy.com/media/3o6fJ8baw3aDgUQnJu/giphy.gif'
-  ]
+    'https://media.giphy.com/media/fAn4sVk9oybFspoXt8/giphy.gif', // janet: it did not work
+    'https://media.giphy.com/media/3o6fJ8baw3aDgUQnJu/giphy.gif', // michael: what the fork is happening
+    'https://media.giphy.com/media/jUuPHMr7mdBcPHm6JX/giphy.gif', // tahani: the confidence comees from failure
+    'https://media.giphy.com/media/PjHVAChtJVUuBFNRv8/giphy.gif', // janet: everything okay? nope
+    'https://media.giphy.com/media/xT9IgG14dkJPBCOQQE/giphy.gif', // michael hands on face
+    'https://media.giphy.com/media/3ohhwBd4UEcFPrezT2/giphy.gif', // eleanor: you gotta be forking kidding me
+    'https://media.giphy.com/media/l3mZo5YSJXPi9rtV6/giphy.gif ', //eleanor: pobody's nerfect
+    'https://media.giphy.com/media/3oxHQkaTabM3m3hnsQ/giphy.gif', // eleanor: ah shirt
+    'https://media.giphy.com/media/UQOwUpOSg2SuykKmOh/giphy.gif', // judge: software update, no cancel
+    'https://media.giphy.com/media/2fTOT5PhBUdJbDeVLH/giphy.gif', // chidi: this broke me
+  ],
+  documentation: [
+    'https://media.giphy.com/media/xUOxeQNdT6Qo0E6ces/giphy.gif', // the gang writing
+    'https://media.giphy.com/media/ONYLxKtDCiIYkrDB53/giphy.gif', // eleanor looking at paper
+    'https://media.giphy.com/media/l1KdaL6whHAdjOUHS/giphy.gif', //michael explaning trolley problem
+    'https://media.giphy.com/media/2rACoNuEUWZUlGGXe5/giphy.gif', // janet typing fast
+    'https://media.giphy.com/media/xUOxfkWMfHlP7bDCY8/giphy.gif', // janet flipping through manual
+    'https://media.giphy.com/media/l1KdaCis4G9m3e4Mg/giphy.gif ', // michael blueprint
+  ],
+  duplicate: [
+    'https://media.giphy.com/media/3ohhwnfGjRoOLIMacg/giphy.gif', // monks in red
+    'https://media.giphy.com/media/3ohhwC5rXJZELsUqB2/giphy.gif', // monks in white
+    'https://media.giphy.com/media/d8dJD3JjzILa60aaaT/giphy.gif', // janets
+  ],
+  enhancement: [
+    'https://media.giphy.com/media/gfx8b5CU2xcBNpbor9/giphy.gif', // chidi: don't look at me i'm just the idea guy
+    'https://media.giphy.com/media/cjzaD2SBabbl31xXhS/giphy.gif', // shawn: new ideas are gross. they sicken me
+    'https://media.giphy.com/media/S8Bnf6KByXDly5VCzq/giphy.gif', // tahani: there's a lot to unpack here
+    'https://media.giphy.com/media/3ohs80ubv3iyojPquY/giphy.gif', // jason thumbs up
+    'https://media.giphy.com/media/3o7aCU8yh7flIPBvy0/giphy.gif', // jason another qt thing
+    'https://media.giphy.com/media/mFYS4Z8K6C6Cx79SN1/giphy.gif', // chidi: oooooh
+  ],
+  good_first_issue: [
+    'https://media.giphy.com/media/Tfuwketx2PdfSgcbki/giphy.gif', // disco janet: solid
+    'https://media.giphy.com/media/eH45SLazJpXjqQ2WXc/giphy.gif', // michael: it's gonna take a lot of work but the work is the fun part guys
+    'https://media.giphy.com/media/xThtalkAxYnZd94uB2/giphy.gif', // michael: okay here we go
+    'https://media.giphy.com/media/UUgTEncAfGQIcNmAk8/giphy.gif', // welcome, everything is fine
+  ],
+  help_wanted: [
+    'https://media.giphy.com/media/3ohs86Ey2s0UVydK6Y/giphy.gif', // tahani: let's chat, shall we?
+    'https://media.giphy.com/media/xT9IgCA0oc92ufQRj2/giphy.gif', // tahani: hands on mouth
+  ],
+  invalid: [
+    'https://media.giphy.com/media/McbX9H1TM2qM8LgCUI/giphy.gif', // shawn: shut up glenn
+    'https://media.giphy.com/media/Ke2ZXWOpu2dGp4sQnM/giphy.gif', // bad janet: shut up glenn
+    'https://media.giphy.com/media/PhIgwQE5OtQzSICp44/giphy.gif', // neutral janet: end of conversation
+    'https://media.giphy.com/media/etUt73tGMTPnuvUtPA/giphy.gif', // chidi: the world is empty
+    'https://media.giphy.com/media/l3mZrLxM4iZaQlvNe/giphy.gif', //eleanor icky face
+    'https://media.giphy.com/media/lQ0urTlnFFyYZL58LO/giphy.gif', // judge: earth is cancelled
+    'https://media.giphy.com/media/WvRgIduWpPWLpGc640/giphy.gif', // judge: earth is a mess yall
+    'https://media.giphy.com/media/3eTTw1qpvq666ulbSd/giphy.gif', // mindy: you know what, i don't care
+  ],
+  question: [
+    'https://media.giphy.com/media/MBfL0f60wUdbp2qKsl/giphy.gif', // janet: all questions are important to me
+    'https://media.giphy.com/media/iEpR0w8v7wSAooMnkv/giphy.gif', // tahani: compromise is always possible
+    'https://media.giphy.com/media/WO5cuCVKjwUiOPVJVW/giphy.gif', // jason: if you're a devil, how come you're not wearing prada?
+    'https://media.giphy.com/media/QslrPJCMRpbthg0SxC/giphy.gif', // jason: if you ever wanna talk just know that i'm here for you, girl
+    'https://media.giphy.com/media/xT0xeK2rDKBfXSuav6/giphy.gif', // eleanor: hey robot slave lady?
+    'https://media.giphy.com/media/VFGIKWcPiVpy4OOb1B/giphy.gif', // janet: i've never said this before, but whaaat?
+  ],
+  wontfix: [
+    'https://media.giphy.com/media/3o7aCZGl9RgEfXEXza/giphy.gif', // vicky: you can all suck it
+    'https://media.giphy.com/media/UPXmPHe0S5r03maowy/giphy.gif', // michael: it's hard when things end
+    'https://media.giphy.com/media/JOKikcIpfff5aHilrW/giphy.gif', // chidi floating in swimming pool
+    'https://media.giphy.com/media/JshOE3wIDIpfeJR0ji/giphy.gif', // bad janet: byeee
+    'https://media.giphy.com/media/3ohs86Ey2s0UVydK6Y/giphy.gif', // janet: bye guys
+    'https://media.giphy.com/media/Mo0VN6NVzyiUCKtnSI/giphy.gif', // shawn: bye bitch
+  ],
 }
-
-
-// Label	Description
-// bug	Indicates an unexpected problem or unintended behavior
-// documentation	Indicates a need for improvements or additions to documentation
-// duplicate	Indicates similar issues or pull requests
-// enhancement	Indicates new feature requests
-// good first issue	Indicates a good issue for first-time contributors
-// help wanted	Indicates that a maintainer wants help on an issue or pull request
-// invalid	Indicates that an issue or pull request is no longer relevant
-// question	Indicates that an issue or pull request needs more information
-// wontfix	Indicates that work won't continue on an issue or pull request
-
-// bug
-// https://media.giphy.com/media/fAn4sVk9oybFspoXt8/giphy.gif - janet: it did not work
-// https://media.giphy.com/media/3o6fJ8baw3aDgUQnJu/giphy.gif - michael: what the fork is happening
-// https://media.giphy.com/media/jUuPHMr7mdBcPHm6JX/giphy.gif - tahani: the confidence comees from failure
-
-// documentation
-// https://media.giphy.com/media/xUOxeQNdT6Qo0E6ces/giphy.gif - the gang writing
-// https://media.giphy.com/media/ONYLxKtDCiIYkrDB53/giphy.gif - eleanor looking at paper
-// https://media.giphy.com/media/l1KdaL6whHAdjOUHS/giphy.gif - michael explaning trolley problem
-// https://media.giphy.com/media/2rACoNuEUWZUlGGXe5/giphy.gif - janet typing fast
-
-// duplicate
-// https://media.giphy.com/media/3ohhwnfGjRoOLIMacg/giphy.gif - monks in red
-// https://media.giphy.com/media/3ohhwC5rXJZELsUqB2/giphy.gif - monks in white
-
-// enhancement
-// https://media.giphy.com/media/gfx8b5CU2xcBNpbor9/giphy.gif - chidi: don't look at me i'm just the idea guy
-// https://media.giphy.com/media/cjzaD2SBabbl31xXhS/giphy.gif - shawn: new ideas are gross. they sicken me
-// https://media.giphy.com/media/S8Bnf6KByXDly5VCzq/giphy.gif - tahani: there's a lot to unpack here
-
-// good first issue
-// https://media.giphy.com/media/Tfuwketx2PdfSgcbki/giphy.gif - disco janet: solid
-
-// help wanted
-
-
-
-// invalid
-// https://media.giphy.com/media/McbX9H1TM2qM8LgCUI/giphy.gif - shawn: shut up glenn
-// https://media.giphy.com/media/Ke2ZXWOpu2dGp4sQnM/giphy.gif - bad janet: shut up glenn
-
-
-// question
-// https://media.giphy.com/media/MBfL0f60wUdbp2qKsl/giphy.gif - janet: all questions are important to me
-// https://media.giphy.com/media/iEpR0w8v7wSAooMnkv/giphy.gif - tahani: compromise is always possible
-// https://media.giphy.com/media/WO5cuCVKjwUiOPVJVW/giphy.gif - jason: if you're a devil, how come you're not wearing prada?
-// https://media.giphy.com/media/QslrPJCMRpbthg0SxC/giphy.gif - jason: if you ever wanna talk just know that i'm here for you, girl
-
-
-// wontfix
-// https://media.giphy.com/media/3o7aCZGl9RgEfXEXza/giphy.gif - vicky: you can all suck it
-// https://media.giphy.com/media/UPXmPHe0S5r03maowy/giphy.gif - michael: it's hard when things end
-// https://media.giphy.com/media/JOKikcIpfff5aHilrW/giphy.gif - chidi floating in swimming pool
-// https://media.giphy.com/media/JshOE3wIDIpfeJR0ji/giphy.gif - bad janet: byeee
